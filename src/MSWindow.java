1bb31c011488d6c9d945a6ab24154e138dcd4480
rupaligangarde <rupaligangarde@in-rupaligangarde.local>
2016-05-03 15:48:22 +0530

    added Window interface + concrete classes

---
+++ src/MSWindow.java
@@ -0,0 +1,11 @@
+
+public class MSWindow implements Window{
+
+    public void setTitle() {
+
+    }
+
+    public void repaint() {
+
+    }
+}
---
+++ src/MacWindow.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class MacWindow implements Window {
+
+    public void setTitle() {
+
+    }
+
+    public void repaint() {
+
+    }
+}
